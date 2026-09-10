acac68ffed719ebec584251cac6fdc9dd67688b5
Kirill <zagorovskiy527@gmail.com>
2026-09-10 15:13:09 +0700

    Добавление координат

--- 1задание.py
+++ 1задание.py
@@ -1,11 +1,14 @@
 class Planet:
-    def __init__(self, name, date, radius):
+    def __init__(self, name, date, radius, x, y):
         self.name = name
         self.date = date
         self.radius = radius
+        self.x = x          
+        self.y = y          
     
     def __str__(self):
-        return f"Планета: {self.name}, дата открытия: {self.date}, радиус: {self.radius}"
+        return (f"Планета: {self.name}, дата открытия: {self.date}, "
+                f"радиус: {self.radius}, координаты: ({self.x}, {self.y})")
 
 
 def parse_planet(text):
@@ -18,13 +21,16 @@ def parse_planet(text):
     
     date = parts[1]
     radius = float(parts[2])
+    x = float(parts[3])     
+    y = float(parts[4])    
     
-    return Planet(name, date, radius)
+    return Planet(name, date, radius, x, y)
 
-with open('input.txt') as file:
-    for line in file:                
-        line = line.strip()          
-        if not line:                 
+
+with open('input.txt', encoding='utf-8') as file:
+    for line in file:
+        line = line.strip()
+        if not line:
             continue
-        planet = parse_planet(line)  
-        print(planet)               
+        planet = parse_planet(line)
+        print(planet)
