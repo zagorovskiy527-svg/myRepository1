3dace1e15e2dd65ae7cace0041c2ad617c124bdc
Kirill <zagorovskiy527@gmail.com>
2026-09-10 14:49:29 +0700

    с файлом

--- 1задание.py
+++ 1задание.py
@@ -7,21 +7,24 @@ class Planet:
     def __str__(self):
         return f"Планета: {self.name}, дата открытия: {self.date}, радиус: {self.radius}"
 
+
 def parse_planet(text):
     start = text.find('"')
     end = text.find('"', start + 1)
     name = text[start + 1:end]
     
     rest = text[:start] + text[end + 1:]
-    
     parts = rest.split()
     
-    date = parts[1]  
+    date = parts[1]
     radius = float(parts[2])
     
     return Planet(name, date, radius)
 
-
-text = 'Земля "Земля" 2024.01.15 6371.0'
-planet = parse_planet(text)
-print(planet)
+with open('input.txt') as file:
+    for line in file:                
+        line = line.strip()          
+        if not line:                 
+            continue
+        planet = parse_planet(line)  
+        print(planet)               
